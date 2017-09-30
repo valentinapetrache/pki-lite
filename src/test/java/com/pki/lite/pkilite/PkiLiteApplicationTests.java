ae048423a04f431f4e2ae476d272675adfae0353
tiberiupetrache <tiberiu.petrache@gmail.com>
2017-09-30 16:47:39 +0300

    first commit

---
+++ src/test/java/com/pki/lite/pkilite/PkiLiteApplicationTests.java
@@ -0,0 +1,16 @@
+package com.pki.lite.pkilite;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PkiLiteApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
